a9a8aa1c654bd7f630c4903a98b91b6b3a4e6f5f
mvnsib <mvnsib@gmail.com>
2021-01-08 16:16:40 +0000

    without test

--- APIMiniProject/APIMiniProject/Services/HTTPManager/SteamCallManager.cs
+++ APIMiniProject/APIMiniProject/Services/HTTPManager/SteamCallManager.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace APIMiniProject
 {
-    class SteamCallManager
+    public class SteamCallManager
     {
+        private readonly IRestClient _client;
+
+        public SteamCallManager()
+        {
+            _client = new RestClient(SteamConfigReader.BaseUrl);
+        }
+        
+        public string GetNews()
+        {
+            string interfaceName = "ISteamUser";
+            string methodName = "GetNewsForApp";
+            string version = "v0002";
+
+            var request = new RestRequest($"{interfaceName}/{methodName}/{version}/?appid=648350&count=3&maxlength=300&format=json");
+
+            var response = _client.Execute(request, Method.GET);
+
+            return response.Content;
+        }
     }
 }
--- APIMiniProject/APIMiniProject/Services/SteamService.cs
+++ APIMiniProject/APIMiniProject/Services/SteamService.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace APIMiniProject
 {
-    class SteamService
+    public class SteamService
     {
+        public SteamCallManager steamCallManager { get; set; } = new SteamCallManager();
+
+        public SteamLatestDTO steamLatestDTO { get; set; } = new SteamLatestDTO();
+
+        public string LiveNews { get; set; }
+
+        public JObject Json_News { get; set; }
+
+        public SteamService()
+        {
+            LiveNews = steamCallManager.GetNews();
+            //Json_News = JsonConvert.DeserializeObject<JObject>(LiveNews);
+            steamLatestDTO.DeserializeNews(LiveNews);
+        }
     }
 }
