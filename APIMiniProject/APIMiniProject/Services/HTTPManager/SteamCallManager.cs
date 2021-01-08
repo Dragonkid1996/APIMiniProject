c3adbddd0ac19780a02b69cc925adcc78fc4d54a
Dragonkid1996 <djnewell@btinternet.com>
2021-01-08 14:31:48 +0000

    Added code

--- APIMiniProject/APIMiniProject/Services/HTTPManager/SteamCallManager.cs
+++ APIMiniProject/APIMiniProject/Services/HTTPManager/SteamCallManager.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using RestSharp;
 
 namespace APIMiniProject
 {
-    class SteamCallManager
+    public class SteamCallManager
     {
+        private readonly IRestClient _client;
+
+        public SteamCallManager()
+        {
+            _client = new RestClient(SteamConfigReader.BaseUrl);
+        }
+
+        public string GetAchievements()
+        {
+            string interfaceName = "ISteamUser";
+            string methodName = "GetGlobalAchievementPercentagesForApp";
+            string version = "v0002";
+
+            var request = new RestRequest($"{interfaceName}/{methodName}/{version}/?gameid=440&format=json");
+            var response = _client.Execute(request, Method.GET);
+            return response.Content;
+        }
+
+        internal string GetLatestAchievements()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- APIMiniProject/APIMiniProject/SteamConfigReader.cs
+++ APIMiniProject/APIMiniProject/SteamConfigReader.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
 namespace APIMiniProject
 {
-    class SteamConfigReader
+    public class SteamConfigReader
     {
+        public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+        public static readonly string ApiKey = ConfigurationManager.AppSettings["api_key"];
+        public static readonly string ApiUrlMod = ConfigurationManager.AppSettings["url_access_mod"];
     }
 }
