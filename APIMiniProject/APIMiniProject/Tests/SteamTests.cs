ff6223d3c064d5038b5444932caef3b4ae1f600b
Shaq10 <shaquille096@gmail.com>
2021-01-09 15:12:50 +0000

    Additional methods and tests

--- APIMiniProject/APIMiniProject/Tests/SteamTests.cs
+++ APIMiniProject/APIMiniProject/Tests/SteamTests.cs
@@ -11,5 +11,21 @@ namespace APIMiniProject
         {
             Assert.That(_steamService.SteamLatestDTO.LatestNews.appnews.appid, Is.EqualTo("648350"));
         }
+
+        [Test]
+        public void TitleContansJurassicWorldEvolution() {
+            Assert.That(_steamService.Json_News["appnews"]["newsitems"][0]["title"].ToString(), Does.Contain("Jurassic World Evolution"));
+        }
+
+        //Tests if first news item contains 11 fields
+        [Test]
+        public void NewsItemContains11Fields() {
+            Assert.That(_steamService.FieldCount(), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void NewsItemsFieldsAreNotEmpty() {
+            Assert.That(_steamService.FieldNotEmpty, Is.EqualTo(true));
+        }
     }
 }
